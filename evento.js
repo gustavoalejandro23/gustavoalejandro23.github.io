eda1c285553adb6600b9890bec882dbb8469ed8d
gustavoalejandro23 <127347164+gustavoalejandro23@users.noreply.github.com>
2023-03-08 17:21:48 -0300

    initial commit

---
+++ evento.js
@@ -0,0 +1,6 @@
+document.getElementById('boton_color').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'black';
+})
+document.getElementById('restablecer').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'lightblue';
+})
